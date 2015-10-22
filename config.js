355f28e8ad604847e20ada8fc6d40aa83ca0b0b1
Ubuntu <akrotech@purplehexblog.purplehexblog.b9.internal.cloudapp.net>
2015-10-22 04:45:37 +0000

    Proper local DB config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
 	/*
 	dbConnectionString: 'postgres://ndxqhnhc:ByIp6McOEziNRY42C5-XJQpAPiUq83wh@nutty-custard-apple.db.elephantsql.com:5432/ndxqhnhc',
 	*/
-	dbConnectionString: 'postgres://akrotech:Akrotech2015@purplehexblog:5432/purplehexblog',
+	dbConnectionString: 'postgres://akrotech:Akrotech2015@localhost:5432/purplehexblog',
 	defaultPort: 8888
 };
