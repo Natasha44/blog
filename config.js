955b2dbcc3c942ad33cff10262426a531e37fe08
Aaron McMurter <aaron.mcmurter@gmail.com>
2015-10-22 00:14:10 -0400

    Modified config to connect to VM DB

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	/*
 	dbConnectionString: 'postgres://ndxqhnhc:ByIp6McOEziNRY42C5-XJQpAPiUq83wh@nutty-custard-apple.db.elephantsql.com:5432/ndxqhnhc',
+	*/
+	dbConnectionString: 'postgres://akrotech:Akrotech2015@purplehexblog:5432/purplehexblog',
 	defaultPort: 8888
 };
