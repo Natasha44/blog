a5c4964079060816c2c963cf89929233c0aac137
Aaron McMurter <aaron.mcmurter@gmail.com>
2015-10-22 01:37:52 -0400

    Remove config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	/*
-	dbConnectionString: 'postgres://ndxqhnhc:ByIp6McOEziNRY42C5-XJQpAPiUq83wh@nutty-custard-apple.db.elephantsql.com:5432/ndxqhnhc',
-	*/
-	dbConnectionString: 'postgres://akrotech:Akrotech2015@localhost:5432/purplehexblog',
-	defaultPort: 8888
-};
